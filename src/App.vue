bea3a0286cdaa8ed7603408d4ba526de8117d2bd
DKH <derek.hawryluk@edu.sait.ca>
2024-03-11 10:17:50 -0600

    fixing file structure

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Card from "./components/Card.vue";
+// import { ref } from "vue";
+</script>
+
+<template>
+  <div id="app">
+    <h2 v-bind:id="headingId">Learning Vue!</h2>
+    <Card />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="card">Card Content</div>
+</template>
+
+<style scoped>
+.card {
+  background-color: black;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.3);
+  padding: 16px;
+  margin-bottom: 20px;
+  width: 200px;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.5);
+}
+</style>
